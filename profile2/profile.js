489e7c24632797373c06612bff1aa61e8dba9193
n1keblyat <133787179+n1keblyat@users.noreply.github.com>
2023-05-24 02:52:35 +0300

    Add files via upload

---
+++ profile2/profile.js
@@ -0,0 +1,6 @@
+/* Когда пользователь нажимает на кнопку,
+переключение между скрытием и отображением раскрывающегося содержимого */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
