6ca6d2701d62b3bcf5077acc722f21761d458ded
matvey <136252403+govNuke3001@users.noreply.github.com>
2023-06-11 19:23:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# ОВРССС
+y1 = ''
+y11 = 'sperma'
+if ("12Б" or ("13Б" and "13В") or ("14Б" and "14В") or "16Б" or "17Б" or "18Б" or "19Б" or ("9Б" and "9В" and "10Б") \
+or (("9В" and '5Б' and '8В' and '8Г' and '8Д') and (('21В' and '21Г' and '21Д' and '21Е') or ('22Е' and '22Ж'))) \
+or '20А' or ('СКГЛД' and ('5Б' or ('8В' and '8Г' and '8Д') or ("9Б" and "9В"))) \
+or ('СКГЛВ' and ('5Б' or ('8В' and '8Г' and '8Д') or ("9Б" and "9В"))) or ('1Б' and "4Е" and "4Ж" and "5А" and "5В" and \
+"8Д" and "21Д" and "21Е") or ("1Б" and "4Е" and "4Ж" and "5Б" and "8Г" and '21Б' and '21В' and '21Г' and '21Д' and '21Е') \
+or ("1Б" and "4Е" and "4Ж" and "5Б" and "8Д") or ("1Б" and "4Е" and "4Ж" and "5Б" and "8В" and "21Д" and "21Е") \
+or ("1А" and "4В" and "5Б" and "8Г" and "21Д" and "21Е") or ("1А" and "4В" and "5Б" and "8Д" and '21В' and '21Г' and '21Д' and '21Е') \
+or ("1А" and "4Г" and "5Б" and "8В" and "21Д" and "21Е") or ("1А" and "4Г" and "5Б" and "8Г" and '21В' and '21Г' and '21Д' and '21Е') \
+or ("1А" and "4Г" and "5Б" and "8Д") or ("1А" and "4Д" and "5Б" and "8А" and "8Б" and "21Д" and "21Е") \
+or ("1А" and "4Д" and "5Б" and '8В' and '8Г' and '8Д') or ("1А" and "4Е" and "4Ж" and "5Б" and "8А" and "8Б" and '21Б' and '21В' and '21Г' and '21Д' and '21Е') \
+or ("1А" and "4Е" and "4Ж" and "5Б" and '8В' and '8Г' and '8Д') or ("1А" and "4Г" and "5А" and "5В" and "8Д" and "21Д" and "21Е") \
+or ("1А" and "4Д" and "4Е" and "4Ж" and "8Г" and "21Д" and "21Е") or ("1А" and "4Д" and "5А" and "5В" and "8Д" and '21В' and '21Г' and '21Д' and '21Е') \
+or ("1А" and "4Е" and "4Ж" and "4Е" and "4Ж" and "8В" and "21Д" and "21Е") or ("1А" and "4Е" and "4Ж" and "5А" and "5В" and "8Г" and '21В' and '21Г' and '21Д' and '21Е') \
+or ("1А" and "4Е" and "4Ж" and "5А" and "5В" and '8Д')) in y1:
+   y11 = 'Очень высокий риск сердечно-сосудистых осложнений – более 10% в течение 10 лет. \
+Рекомендуется снижение ЛПНП как минимум, на ≥50% от исходного уровня, цель <1,4 ммоль/л (<55 мг/дл). При повторном сосудистом событии в течение последних 2 лет целевой уровень ЛПНП <1 ммоль/л (<40 мг/дл).'
+print(y11)
+
+
+
